8fd94ae4d975863684c339a3cb3143a146b34c3a
adesaleneuve <adesaleneuve@info-305-18x.iut.bx1>
2019-04-29 18:06:46 +0200

    Debut de fx du menu

---
+++ src/boardgameproject/Menu/MenuController.java
@@ -0,0 +1,19 @@
+package boardgameproject.Menu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class MenuController {
+
+    @FXML
+    void onPlayClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onQuitClick(ActionEvent event) {
+
+    }
+
+}
