dceee3ed86ac63a808dcbaa9e602dd740a5f3085
adesaleneuve <adesaleneuve@info-305-18x.iut.bx1>
2019-05-06 14:15:50 +0200

    Reste du MenuController

--- src/boardgameproject/Round.java
+++ src/boardgameproject/Round.java
@@ -6,6 +6,9 @@
 package boardgameproject;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -13,13 +16,19 @@ import javafx.stage.Stage;
  * @author mlecoeuvre
  */
 public class Round extends Application {
+
     private int nbTurnMax;
     private int nbTurn;
     private int nbPlayer;
-    
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
- 
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu/Menu.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
@@ -28,5 +37,5 @@ public class Round extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
