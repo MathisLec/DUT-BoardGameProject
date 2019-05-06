63c1d626599260fcc207c20629227609afb88079
adesaleneuve <adesaleneuve@info-305-18x.iut.bx1>
2019-05-06 14:15:07 +0200

    Ecran du MenuController

--- src/boardgameproject/Menu/MenuController.java
+++ src/boardgameproject/Menu/MenuController.java
@@ -1,19 +1,39 @@
 package boardgameproject.Menu;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-
-public class MenuController {
-
+public class MenuController implements Initializable {
+    
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button quitButton;
+    @FXML
+    private Label titre;
+    
     @FXML
     void onPlayClick(ActionEvent event) {
-        int abv;
+        
     }
-
+    
     @FXML
     void onQuitClick(ActionEvent event) {
-
+        System.exit(0);
     }
-
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        titre.setText("JEU PLATEAU BATIMENTS");
+    }
+    
+    public void aaaa() {
+        
+    }
+    
 }
